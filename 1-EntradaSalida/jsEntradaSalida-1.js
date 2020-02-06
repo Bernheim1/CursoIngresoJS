28f192b6bfa0a88ef170206dea6b2cfb68117fdf
Bernheim1 <60743472+Bernheim1@users.noreply.github.com>
2020-02-06 11:13:00 -0300

    Ejercicio 1- Hecho
    Como usar alert, valores literales y variables

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,29 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
+
+
+	var primerprecio;
+	var segundoPrecio;
+	var apellido="nadie";
+
+	apellido= "Perez"
+	alert(apellido)
+
+
+
+	// Si esta entre dobles comillas es un valor literal
+	//alert("Tal cual"); 
+	//alert(apellido); = no va entre comillas porque es una variable
+	//alert = sale vacio 
+	//alert(preciopri) = no anda el boton porque no existe
+
+/* 	
+	Alert recibe un texto y lo muestra
+	Puede ser literal o una variable
+*/
+
 }
+
+
 
