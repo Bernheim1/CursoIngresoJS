a9dd943c6c2ac2b8abfd13fc2a0323635c58b11b
Bernheim1 <60743472+Bernheim1@users.noreply.github.com>
2020-02-06 12:10:45 -0300

    Ejercicio 2 - Hecho
    Prompt y consolelog

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,17 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
-}
+	var nombre = prompt("Porfavor ingrese su nombre");
+	console.log ( "su nombre es" + nombre);
 
+
+/* 
+
+	El prompt sirve para pedirle 
+	datos al usuario
+	Console log muestra los datos 
+	que haya pedido en la consola
+	del chrome
+*/
+
+}
