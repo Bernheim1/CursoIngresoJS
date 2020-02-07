94a07889d76bf3ce97a9ad53ca4bfac12d8694dd
Bernheim1 <60743472+Bernheim1@users.noreply.github.com>
2020-02-07 16:58:11 -0300

    Ejercicio 1 parcial - Hecho

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,17 @@
 
 function mostrar()
 {
+	var largo;
+	var ancho;
+	var resultado;
+
+	largo=prompt("Por favor ingrese el largo del rectangulo");
+	largo=parseInt(largo);
+
+	ancho=prompt("Por favor ingrese el ancho del rectangulo");
+	ancho=parseInt(ancho);
+
+	resultado=(largo*2)+(ancho*2);
+	alert(resultado);
 
 }
