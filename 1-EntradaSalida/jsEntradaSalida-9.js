86f0afb67d2ad9f0db52f7a0f74da71c822f92ab
Bernheim1 <60743472+Bernheim1@users.noreply.github.com>
2020-02-07 11:57:40 -0300

    Ejercicio 9 - Hecho
    Sacar porcentaje, sacar aumento, (similar ejercicio 3 parcial)

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,25 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var importe;
+	var porcentaje;
+	var aumento;
+	var resultado;
+
+	importe = document.getElementById('sueldo').value;
+	importe = parseInt(importe);
+
+	porcentaje = 10;
+
+	aumento = importe*porcentaje/100;
+
+	resultado = importe + aumento;
+
+	document.getElementById('resultado').value = resultado;
+
+	console.log(resultado);
+
+
 
 	
 }
