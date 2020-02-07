10c6b8439520bad81c43e21a1ead5986bc3a18cb
Bernheim1 <60743472+Bernheim1@users.noreply.github.com>
2020-02-07 11:09:25 -0300

    Ejercicio 3 - Hecho
    document.getElementById

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,17 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	var nombre;
+	nombre = document.getElementById('elNombre').value;
+	alert(nombre)
+	document.getElementById('elNombre').value=("juan pablo segundo")
+
+
+
+
+
+
+	//ID, solo puede haber uno, si hay mas de uno esta mal programado
 }
 
 
