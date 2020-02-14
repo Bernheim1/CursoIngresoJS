54e9eb38adf716d3fed53dd856cdb6e7f77c0185
Bernheim1 <60743472+Bernheim1@users.noreply.github.com>
2020-02-14 12:24:17 -0300

    Hecho

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,33 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+		var notaRandom;
+
+	notaRandom=Math.floor(Math.random() * 10) + 1;
+	console.log(notaRandom);
+
+
+
+	if(notaRandom>8)
+	{
+
+		alert("Excelente");
+
+	}else
+	{
+		if (notaRandom>3) 
+		{
+
+			alert("Aprobo");
+
+
+		}else
+		{
+			alert("Vamos, la proxima se puede");
+		}
+
+
+	}
 
 }//FIN DE LA FUNCIÓN
+
