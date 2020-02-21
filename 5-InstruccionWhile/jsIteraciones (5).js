b7626c532b70906d368fe56942aec9b6fdbf2453
Bernheim1 <60743472+Bernheim1@users.noreply.github.com>
2020-02-20 22:31:33 -0300

    Casi Hecho

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -2,13 +2,13 @@ function mostrar()
 {
 
 	var sexo;
-
 	sexo = prompt("ingrese f ó m .");
 
+		//(!(sexo=="f"||sexo=="m"))
 	while(sexo!="f"||sexo!="m")
 	{
-		sexo = prompt("ingrese f ó m .");
+		sexo = prompt("ingrese ingrese un valor valido");
 	}
 	document.getElementById('Sexo').value=sexo;
-
-}//FIN DE LA FUNCIÓN
+	alert("fin");
+}
--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,10 +3,23 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	acumulador=prompt("Ingrese un numero por favor");
+	acumulador=parseInt(acumulador);
+
+	while(contador<6)
+	{
+		contador=contador+1;
+		acumulador=prompt("Ingrese otro numero por favor");
+		acumulador=parseInt(acumulador);
+		console.log(contador);
+	}
+	
 
 
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+
+
+	document.getElementById('suma').value=acumulador;
+	document.getElementById('promedio').value=acumulador/5;
 
 }//FIN DE LA FUNCIÓN
