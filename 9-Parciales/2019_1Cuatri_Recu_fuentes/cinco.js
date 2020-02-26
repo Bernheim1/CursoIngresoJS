541c1763254f08700ab05043b8cbf417dccb3c2e
Bernheim1 <60743472+Bernheim1@users.noreply.github.com>
2020-02-25 23:48:35 -0300

    Hecho

--- 9-Parciales/2019_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2019_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,70 @@
 function mostrar()
 {
+	var destino;
+	var cantidadDias;
+	var descuento;
+	var descuentoExtra;
+	var formaPago;
+	var recargo;
+	var precioDia;
+	var precioBase;
+	var precioFinal;
+
+	destino=document.getElementById('selecContinente').value;
+	formaPago=document.getElementById('selecPago').value;
+	cantidadDias=prompt("Por favor ingrese la cantidad de dias");
+	cantidadDias=parseInt(cantidadDias);
+	descuento=0;
+	descuentoExtra=0;
+	recargo=0;
+	precioDia=100;
+
+
+	switch(destino&&formaPago)
+	{
+		case "America":
+		case "Débito":
+			descuentoExtra=10;
+			descuento=15;
+			break;
+		case "África":
+		case "Oceania":
+		case "MercadoPago":
+		case "Efectivo":
+			descuentoExtra=15;
+			descuento=30;
+			break;
+		case "Europa":
+		case "Débito":
+			descuentoExtra=15;
+			descuento=20;
+			break;
+		case "Europa":
+		case "MercadoPago":
+			descuentoExtra=10;
+			descuento=20;
+			break;
+		case "Europa":
+			descuentoExtra=5;
+			descuento=20;
+			break;
+		case "Asia":
+			recargo=20;
+			break;
+		default:
+			case "Cheque": 
+			recargo=15;
+			break;
+
+	}
+
+	descuento=descuento+descuentoExtra;
+
+	precioBase=precioDia*cantidadDias;
+
+	precioFinal=(precioBase-precioBase*descuento/100)+precioBase*recargo/100;
+
+	alert("El precio final del viaje es: "+precioFinal);
+
 
 }
