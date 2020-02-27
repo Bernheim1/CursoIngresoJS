07e3e91d4c4ddc96fe3f10d32f0c1da27a7b8433
Bernheim1 <60743472+Bernheim1@users.noreply.github.com>
2020-02-26 21:48:44 -0300

    Hecho

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -2,15 +2,67 @@ function mostrar()
 {
 
 	var contador=0;
-	//declarar contadores y variables 
-	
-	var respuesta="si";
+	var numeroIngresado;
+	var sumaNegativos=0;
+	var sumaPositivos=0;
+	var cantidadNegativos=0;
+	var cantidadPositivos=0;
+	var cantidadCeros=0;
+	var cantidadPares=0;
+	var promedioPositivos=0;
+	var promedioNegativos=0;
+	var difPosYNeg;
+	var respuesta=true;
 
-	while(respuesta!="no")
+	while(respuesta==true)
 	{
+		contador=contador+1;
+
+		numeroIngresado=prompt("Ingrese un numero");
+		numeroIngresado=parseInt(numeroIngresado);
+
+		while(isNaN(numeroIngresado))
+		{
+			numeroIngresado=prompt("Ingrese un valor valido");
+			numeroIngresado=parseInt(numeroIngresado);
+		}
 		
-	
+		if (numeroIngresado<0)
+		{
+			sumaNegativos=sumaNegativos+numeroIngresado;
+			cantidadNegativos=cantidadNegativos+1;
+		}
+		if (numeroIngresado>0) 
+		{
+			sumaPositivos=sumaPositivos+numeroIngresado;
+			cantidadPositivos=cantidadPositivos+1;
+		}
+		if (numeroIngresado==0)
+		{
+		 	cantidadCeros=cantidadCeros+1;
+		}
+		if (numeroIngresado%2==0) 
+		{
+			cantidadPares=cantidadPares+1;
+		}
+
+		respuesta=confirm("Presione aceptar para continuar");
 	}
+
+	promedioPositivos=sumaPositivos/cantidadPositivos;
+	promedioNegativos=sumaNegativos/cantidadNegativos;
+	difPosYNeg=sumaPositivos-sumaNegativos
+
+	console.log("Promedio negativos "+promedioNegativos);
+	console.log("Promedio positivos "+promedioPositivos);
+	console.log("Diferencia positivos y negativos "+difPosYNeg);
+	console.log("Suma negativos "+sumaNegativos);
+	console.log("Suma positivos "+sumaPositivos);
+	console.log("Cantidad negativos "+cantidadNegativos);
+	console.log("Cantidad positivos "+cantidadPositivos);
+	console.log("Cantidad pares "+cantidadPares);
+	console.log("Cantidad ceros "+cantidadCeros);
+
 
 
 
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -5,18 +5,46 @@ function mostrar()
 	var minimo;
 	var maximo;
 	var numeroIngresado;
-	var respuesta='si';
+	var respuesta=true;
 
-	while(IsNaN(respuesta=="si"))
+	while(respuesta==true)
 	{
 		contador=contador+1;
-		numeroIngresado=prompt("Por favor ingrese un numero")
+		numeroIngresado=prompt("Por favor ingrese un numero");
 		numeroIngresado=parseInt(numeroIngresado);
 		
-	
+		while(isNaN(numeroIngresado))
+		{
+			numeroIngresado=prompt("Debe ingresar un numero");
+			numeroIngresado=parseInt(numeroIngresado);
+		}
+		
+		if (contador==1)
+		 {
+				 	maximo=numeroIngresado;
+				 	minimo=numeroIngresado;
+		 }
+		 else
+		 {
+		 	if (numeroIngresado>maximo)
+ 			{
+ 					maximo=numeroIngresado;
+			}
+		 	if (numeroIngresado<minimo) 
+			{
+		 			minimo=numeroIngresado;
+		 	} 
+		 }	
+
+
+		respuesta=confirm("Presione OK para continuar");
+		
+
+		
 	}
+		document.getElementById('maximo').value=maximo;
+		document.getElementById('minimo').value=minimo;
+}
 
 
-
-
-}//FIN DE LA FUNCIÓN
+//FIN DE LA FUNCIÓN
