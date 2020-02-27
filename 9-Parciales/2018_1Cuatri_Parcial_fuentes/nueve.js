1cbd3f5852602af2f53397ccdad35de780808bd7
Bernheim1 <60743472+Bernheim1@users.noreply.github.com>
2020-02-26 23:12:55 -0300

    Casi hecho

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,37 @@
 function mostrar()
 {
+	var contador=0;
+	var marca;
+	var peso;
+	var temperatura=0;
+	var temperaturaPar=0;
+	var marcaMasPesado;
+	var productosMenosCero=0;
+	var	promedioPeso;
+	var pesoMaximo;
+	var pesoMinimo;
+	var respuesta=true;
+
+	while(respuesta=true)
+	{
+		marca=prompt("Ingrese la marca");
+		
+		peso=prompt("Ingrese el peso");
+		peso=parseInt(peso);
+
+		while(isNaN(peso))
+		{
+			peso=prompt("Ingrese un valor valido");
+			peso=parseInt(peso);
+		}
+		
+		temperatura=prompt("Ingrese la temperatura (Entre -30 y 30 grados");
+		
+
+
+	}
+
+
+
 
 }
