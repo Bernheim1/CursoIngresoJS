d24a02705a9c81b220ccb936bd257a3ae7dc6575
Bernheim1 <60743472+Bernheim1@users.noreply.github.com>
2020-02-29 01:42:32 -0300

    Practica parcial curso de ingreso

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,17 +1,112 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
+	var nombreHuesped;
+	var cantidadPersonasIngresadas;
+	var cantidadDiasIngresados;
+	var formaDePago;
+	var contador=0;
+	var respuesta=true;
+	var nombreHuespedMasPersonas;
+	var mayorCantidadPersonas;
+	var personasMasDias;
+	var mayorCantidadDias;
+	var contadorEfectivo=0;
+	var contadorTarjeta=0;
+	var contadorQR=0;
+	var formaDePagoMasUtilizada;
+	var promedioCantidadDias=0;
+
+	while(respuesta==true)
+	{
+		contador=contador+1;
+
+		do
+		{
+			nombreHuesped=prompt("Ingrese el nombre");
+		}while(!isNaN(nombreHuesped));
+
+		do
+		{
+			cantidadPersonasIngresadas=prompt("Ingrese el la cantidad de personas");
+			cantidadPersonasIngresadas=parseInt(cantidadPersonasIngresadas)
+		}while(isNaN(cantidadPersonasIngresadas));
+
+		do
+		{
+			cantidadDiasIngresados=prompt("Ingrese el la cantidad de dias");
+			cantidadDiasIngresados=parseInt(cantidadDiasIngresados)
+		}while(isNaN(cantidadDiasIngresados));
+
+		do
+		{
+			formaDePago=prompt("Ingrese la forma de pago (efectivo,tarjeta o QR)");
+		}while(formaDePago!="efectivo"&&formaDePago!="tarjeta"&&formaDePago!="QR");
 
 
-	var primerprecio;
-	var segundoPrecio;
-	var apellido="nadie";
+		if (contador==1)
+		{
+			mayorCantidadPersonas=cantidadPersonasIngresadas;
+			mayorCantidadDias=cantidadDiasIngresados;
+			nombreHuespedMasPersonas=nombreHuesped;
+			personasMasDias=cantidadPersonasIngresadas;
+		}
+		if (cantidadPersonasIngresadas>mayorCantidadPersonas)
+		{
+			nombreHuespedMasPersonas=nombreHuesped;
+			mayorCantidadPersonas=cantidadPersonasIngresadas;
+		}
+		if (cantidadDiasIngresados>mayorCantidadDias) 
+		{
+			personasMasDias=cantidadPersonasIngresadas;
+			mayorCantidadDias=cantidadDiasIngresados;
+		}
 
-	testeando git
+		switch(formaDePago)
+		{
+			case "efectivo":
+				contadorEfectivo=contadorEfectivo+1;
+				break;
+			case "tarjeta":
+				contadorTarjeta=contadorTarjeta+1;
+				break;
+			case "QR":
+				contadorQR=contadorQR+1;
+				break;
+		}
+
+		promedioCantidadDias=promedioCantidadDias+cantidadDiasIngresados;
+
+		respuesta=confirm("Presione aceptar para continuar");
+	}
+
+		if (contadorEfectivo>contadorTarjeta&&contadorEfectivo>contadorQR)
+		 {
+		 	formaDePagoMasUtilizada="Efectivo";
+		 }
+		else
+		{
+			if (contadorTarjeta>contadorEfectivo&&contadorTarjeta>contadorQR)
+			 {
+			 	formaDePagoMasUtilizada="Tarjeta";
+			 }
+			 else
+			 {
+			 	formaDePagoMasUtilizada="QR";
+			 }
+		}
+
+		document.write("El huesped que trajo mas personas en una reserva fue: "+nombreHuespedMasPersonas+"<br>");
+		document.write("La cantidad de personas que se quedo mas dias fue: "+personasMasDias+"<br>");
+		document.write("La forma de pago mas utilizada fue: "+formaDePagoMasUtilizada+"<br>");
+		document.write("El promedio de cantidad de dias por reserva: "+promedioCantidadDias/contador);
+
+	
+}
 
 
 
-	// Si esta entre dobles comillas es un valor literal
+// Si esta entre dobles comillas es un valor literal
 	//alert("Tal cual"); 
 	//alert(apellido); = no va entre comillas porque es una variable
 	//alert = sale vacio 
@@ -21,8 +116,3 @@ function mostrar()
 	Alert recibe un texto y lo muestra
 	Puede ser literal o una variable
 */
-
-}
-
-
-
--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,8 +2,107 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombre = prompt("Porfavor ingrese su nombre");
-	console.log ( "su nombre es" + nombre);
+	var temperatura;
+	var nombre;
+	var sexo;
+	var edad;
+	var respuesta=true;
+	var contador=0;
+	var temperaturaMasAlta;
+	var nombreTempMasAlta;
+	var sexoTempMasAlta;
+	var masJoven;
+	var masViejo;
+	var temperaturaMasJoven;
+	var	temperaturaMasViejo;
+	var promedioTotal=0;
+	var promedioMujeres=0;
+	var cantidadMujeres=0;
+
+	while(respuesta==true)
+	{
+		contador=contador+1;
+
+
+		do
+		{
+			nombre=prompt("Ingrese el nombre");
+		}while(!isNaN(nombre));
+
+		do
+		{
+			temperatura=prompt("Ingrese la temperatura");
+			temperatura=parseInt(temperatura);
+		}while(isNaN(temperatura)||temperatura>45||temperatura<15);
+
+		do
+		{
+			sexo=prompt("Ingrese el sexo");
+		}while(sexo!="m"&&sexo!="f");
+
+		do
+		{
+			edad=prompt("Ingrese la edad");
+			edad=parseInt(edad);
+		}while(isNaN(edad)||edad>110||edad<0);
+
+		if (contador==1)
+		{
+		 	temperaturaMasAlta=temperatura;
+		 	nombreTempMasAlta=nombre;
+			sexoTempMasAlta=temperatura;
+			temperaturaMasViejo=temperatura;
+			temperaturaMasJoven=temperatura;
+			masJoven=edad;
+			masViejo=edad;
+		}
+		else
+		{
+			if (temperatura>temperaturaMasAlta)
+			{
+				temperaturaMasAlta=temperatura;
+				nombreTempMasAlta=nombre;
+				sexoTempMasAlta=sexo;
+			}
+			if (temperatura>37||edad<59||edad>13)
+			{
+				alert("Riesgo:"+nombre);
+			}
+			if (edad<masJoven)
+			{
+			 	masJoven=edad;
+			 	temperaturaMasJoven=temperatura;
+			}
+			if (edad>masViejo)
+			{
+				masViejo=edad;
+				temperaturaMasViejo=temperatura;
+			}
+			if (sexo=="f")
+			{
+			 	cantidadMujeres=cantidadMujeres+1;
+			 	promedioMujeres=promedioMujeres+edad;
+			}
+
+
+		}	
+
+		promedioTotal=promedioTotal+edad;
+
+
+		respuesta=confirm("Presione aceptar para continuar");
+
+	}
+
+	document.write("El nombre de la persona con temperatura mas alta es: "+nombreTempMasAlta+" y su sexo es: "+sexoTempMasAlta+"<br>");
+	document.write("La temperatura del paciente mas joven es: "+temperaturaMasJoven+"<br>");
+	document.write("La temperatura del paciente mas viejo es: "+temperaturaMasViejo+"<br>");
+	document.write("El promedio de la edad total es: "+promedioTotal/contador+"<br>");
+	document.write("El promedio de la edad de las mujeres es: "+promedioMujeres/cantidadMujeres+"<br>");
+
+}
+
+
 
 
 /* 
@@ -14,5 +113,3 @@ function mostrar()
 	que haya pedido en la consola
 	del chrome
 */
-
-}
